e8cf860ad2f6af84ee785a2b16903b55e80f3378
Idan Mintz <imintz@chromium.org>
2015-04-14 14:30:49 -0400

    Added cordova.js

---
+++ cordova.js
@@ -0,0 +1,11 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('sw.js',{scope:'/cordova-plugin-background-sync/'}).then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    document.dispatchEvent(new Event('deviceready'));
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
